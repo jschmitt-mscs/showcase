c51b633e9d9263c762e2711936826b493942fe98
jschmitt-mscs <er3709pt@go.minnstate.edu>
2021-12-30 20:43:33 -0600

    added ror2 mod

---
+++ ROR2 Mod/JSMods-1/Class1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using BepInEx;
+using EntityStates.BeetleQueenMonster;
+using EntityStates.GolemMonster;
+using IL.EntityStates.GolemMonster;
+using IL.RoR2;
+using R2API;
+using RoR2;
+using UnityEngine;
+using RoR2.Artifacts;
+using JetBrains.Annotations;
+using EntityStates.Engi.EngiWeapon;
+using UnityEngine.Networking;
+
+namespace JSMods_1
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.JSMods.Mod1","JS Test Mod Numero Uno","1.0.0")]
+
+    public class Mod1 : BaseUnityPlugin
+    {
+
+
+        public void Awake()
+        {
+
+
+
+
+            RoR2.Chat.AddMessage("Loaded Mod1: Currently Doing Nothing");
+      
+
+
+            On.EntityStates.GolemMonster.ChargeLaser.OnEnter += (orig, self) =>
+            {
+                EntityStates.GolemMonster.ChargeLaser.baseDuration = 0.5f;
+               
+                orig(self);
+            };
+
+            On.RoR2.Run.BeginStage += (orig, self) =>
+            {
+                for (int i = 0; i < RoR2.Run.instance.stageClearCount; i++)
+                {
+                    RoR2.TeleporterInteraction.instance.AddShrineStack();
+                }
+                orig(self);
+            };
+
+            On.RoR2.CharacterMaster.Respawn += (orig, self, footPosition, rotation, tryToGroundSafely) =>
+            {
+
+
+                
+
+               return orig(self, footPosition, rotation, tryToGroundSafely);
+               
+            };
+
+            On.EntityStates.Huntress.ArrowRain.OnEnter += (orig, self) =>
+            {
+                //code that should be run
+                EntityStates.Huntress.HuntressWeapon.ThrowGlaive.glaiveTravelSpeed += 5;
+                EntityStates.Huntress.HuntressWeapon.ThrowGlaive.maxBounceCount += 1;
+                EntityStates.Huntress.HuntressWeapon.ThrowGlaive.damageCoefficient *= 1.1f;
+                orig(self);
+            };
+
+            RoR2.GlobalEventManager.onCharacterDeathGlobal += GlobalEventManager_onCharacterDeathGlobal;
+        }
+
+
+        private void spawnMonster(string monsterName, int eliteIndex, RoR2.CharacterBody charBody)
+        {
+
+            var transform = charBody.transform;
+
+            GameObject gameObject = RoR2.MasterCatalog.FindMasterPrefab(monsterName);
+            GameObject bodyPrefab = gameObject.GetComponent<RoR2.CharacterMaster>().bodyPrefab;
+            GameObject gameObject2 = UnityEngine.Object.Instantiate<GameObject>(gameObject, transform.position, Quaternion.identity);
+            RoR2.CharacterMaster component = gameObject2.GetComponent<RoR2.CharacterMaster>();
+            NetworkServer.Spawn(gameObject2);
+            component.SpawnBody(bodyPrefab, transform.position, Quaternion.identity);
+
+            RoR2.EliteIndex enumFromPartial = Utilities.GetEnumFromPartial<RoR2.EliteIndex>(eliteIndex.ToString());
+            component.inventory.SetEquipmentIndex(RoR2.EliteCatalog.GetEliteDef(enumFromPartial).eliteEquipmentIndex);
+            component.inventory.GiveItem(ItemIndex.BoostHp, Mathf.RoundToInt((Utilities.GetTierDef(enumFromPartial).healthBoostCoefficient - 1f) * 10f));
+            component.inventory.GiveItem(ItemIndex.BoostDamage, Mathf.RoundToInt((Utilities.GetTierDef(enumFromPartial).damageBoostCoefficient - 1f) * 10f));
+            component.teamIndex = RoR2.TeamIndex.Monster;
+
+
+
+        }
+
+        //when enemy dies, spawn copy of it
+
+        private void GlobalEventManager_onCharacterDeathGlobal(RoR2.DamageReport report)
+        {
+            System.Random rnd = new System.Random();
+            int Odds = rnd.Next(1, 5);
+            if(Odds == 3)
+            {
+                spawnMonster(Utilities.Enemies.LemurianMaster.ToString(), 1, report.victimBody);
+                RoR2.Chat.AddMessage(report.victim.ToString());
+                RoR2.Chat.AddMessage(report.victimBody.name);
+                RoR2.Chat.AddMessage(report.victimMaster.name);
+            }
+        }
+
+    }
+}
+
+
---
+++ ROR2 Mod/JSMods-1/Utilities.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RoR2;
+using UnityEngine;
+using System.Reflection;
+using R2API.Utils;
+
+namespace JSMods_1
+{
+    class Utilities
+    {
+        public static TEnum GetEnumFromPartial<TEnum>(string name)
+        {
+            TEnum[] array = (TEnum[])Enum.GetValues(typeof(TEnum));
+            int num;
+            if (!int.TryParse(name, out num))
+            {
+                foreach (TEnum tenum in array)
+                {
+                    if (Enum.GetName(typeof(TEnum), tenum).ToUpper().Contains(name.ToUpper()))
+                    {
+                        return tenum;
+                    }
+                }
+                return default(TEnum);
+            }
+            if (num >= array.Length)
+            {
+                return default(TEnum);
+            }
+            return array[num];
+        }
+
+        internal static CombatDirector.EliteTierDef GetTierDef(EliteIndex index)
+        {
+            int num = 0;
+            CombatDirector.EliteTierDef[] fieldValue = Reflection.GetFieldValue<CombatDirector.EliteTierDef[]>(typeof(CombatDirector), "eliteTiers");
+            if (index > EliteIndex.None && index < EliteIndex.Count)
+            {
+                for (int i = 0; i < fieldValue.Length; i++)
+                {
+                    for (int j = 0; j < fieldValue[i].eliteTypes.Length; j++)
+                    {
+                        if (fieldValue[i].eliteTypes[j] == index)
+                        {
+                            num = i;
+                        }
+                    }
+                }
+            }
+            return fieldValue[num];
+        }
+
+        public static void spawnMonster(string monsterName, int eliteIndex, RoR2.CharacterBody charBody)
+        {
+
+            var transform = charBody.transform;
+
+            GameObject gameObject = RoR2.MasterCatalog.FindMasterPrefab(monsterName);
+            GameObject bodyPrefab = gameObject.GetComponent<RoR2.CharacterMaster>().bodyPrefab;
+            GameObject gameObject2 = UnityEngine.Object.Instantiate<GameObject>(gameObject, transform.position, Quaternion.identity);
+            RoR2.CharacterMaster component = gameObject2.GetComponent<RoR2.CharacterMaster>();
+            component.SpawnBody(bodyPrefab, transform.position, Quaternion.identity);
+
+            RoR2.EliteIndex enumFromPartial = GetEnumFromPartial<RoR2.EliteIndex>(eliteIndex.ToString());
+            component.inventory.SetEquipmentIndex(RoR2.EliteCatalog.GetEliteDef(enumFromPartial).eliteEquipmentIndex);
+            component.inventory.GiveItem(ItemIndex.BoostHp, Mathf.RoundToInt((GetTierDef(enumFromPartial).healthBoostCoefficient - 1f) * 10f));
+            component.inventory.GiveItem(ItemIndex.BoostDamage, Mathf.RoundToInt((GetTierDef(enumFromPartial).damageBoostCoefficient - 1f) * 10f));
+            component.teamIndex = RoR2.TeamIndex.Monster;
+
+        }
+
+        public enum Enemies
+        {
+            AncientWispMaster,
+            ArchWispMaster,
+            ArtifactShellMaster,
+            BeetleCrystalMaster,
+            BeetleGuardAllyMaster,
+            BeetleGuardMaster,
+            BeetleGuardMasterCrystal,
+            BeetleMaster,
+            BeetleQueenMaster,
+            BellMaster,
+            BisonMaster,
+            BrotherGlassMaster,
+            BrotherHauntMaster,
+            BrotherHurtMaster,
+            BrotherMaster,
+            CaptainMonsterMaster,
+            ClayBossMaster,
+            ClayBruiserMaster,
+            ClaymanMaster,
+            CommandoMonsterMaster,
+            CrocoMonsterMaster,
+            Drone1Master,
+            Drone2Master,
+            DroneBackupMaster,
+            DroneMissileMaster,
+            ElectricWormMaster,
+            EmergencyDroneMaster,
+            EngiBeamTurretMaster,
+            EngiMonsterMaster,
+            EngiTurretMaster,
+            EngiWalkerTurretMaster,
+            EquipmentDroneMaster,
+            FlameDroneMaster,
+            GolemMaster,
+            GrandparentMaster,
+            GravekeeperMaster,
+            GreaterWispMaster,
+            HermitCrabMaster,
+            HuntressMonsterMaster,
+            ImpBossMaster,
+            ImpMaster,
+            JellyfishMaster,
+            LemurianBruiserMaster,
+            LemurianBruiserMasterFire,
+            LemurianBruiserMasterHaunted,
+            LemurianBruiserMasterIce,
+            LemurianBruiserMasterPoison,
+            LemurianMaster,
+            LoaderMonsterMaster,
+            LunarGolemMaster,
+            LunarWispMaster,
+            MageMonsterMaster,
+            MagmaWormMaster,
+            MegaDroneMaster,
+            MercMonsterMaster,
+            MiniMushroomMaster,
+            NullifierMaster,
+            ParentMaster,
+            ParentPodMaster,
+            RoboBallBossMaster,
+            RoboBallMiniMaster,
+            ScavLunar1Master,
+            ScavLunar2Master,
+            ScavLunar3Master,
+            ScavLunar4Master,
+            ScavMaster,
+            ShopkeeperMaster,
+            SquidTurretMaster,
+            SuperRoboBallBossMaster,
+            TitanGoldAllyMaster,
+            TitanGoldMaster,
+            TitanMaster,
+            ToolbotMonsterMaster,
+            TreebotMonsterMaster,
+            Turret1Master,
+            UrchinTurretMaster,
+            VagrantMaster,
+            VultureMaster,
+            WispMaster,
+            WispSoulMaster
+        }
+    }
+}
